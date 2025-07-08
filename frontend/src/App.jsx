b26094e45279843f7e0baf272eb6ee0ef749c63a
yao <ycheng11036@gmail.com>
2025-07-08 12:43:19 -0400

    Sidebar complated without functionalities

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
-import Login from './pages/login/Login.jsx'
-import SignUp from './pages/signup/SignUp.jsx'
+import Login from './pages/login/Login'
+import SignUp from './pages/signup/SignUp'
+import Home from './pages/homepage/Home'
 
 function App() {
 
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <SignUp />
-
+      {/* <SignUp /> */}
+      <Home />
     </div>
   )
 }
--- frontend/src/pages/homepage/Home.jsx
+++ frontend/src/pages/homepage/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Sidebar from '../../components/Sidebar/Sidebar'
 
 const Home = () => {
   return (
-    <div className='flex sm:h[450px] md:h[550px] rounded-lg overflow-hidden bg-gray-400/20 bg-clip-padding backdrop-filter backdrop-blur-lg'>
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400/20 bg-clip-padding backdrop-filter backdrop-blur-lg'>
       <Sidebar />
-      <MessageContainer />
+      {/* <MessageContainer /> */}
     </div>
   )
 }
