10124696452f72172371426ca9b187dfd162fcb5
yao <ycheng11036@gmail.com>
2025-07-10 15:16:56 -0400

    Added search functionality for conversations and display time when the message is sent

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,13 +1,15 @@
 import React, { Profiler } from 'react'
 import { useAuthContext } from '../../context/AuthContext'
 import useConversation from '../../zustand/useConversation';
+import { extractTime } from '../../../../backend/utils/extractTime';
 
 const Message = ({message}) => {
   const {authUser} = useAuthContext();
   const {selectedConversation} = useConversation();
   const fromMe = message.senderId === authUser._id;
+  const formattedTime = extractTime(message.createdAt);
   const chatClassName = fromMe ? 'chat-end' : 'chat-start';
-  const profilePic = fromMe ? authUser.profielPic : selectedConversation.profielPic;
+  const profilePic = fromMe ? authUser.profielPic : selectedConversation?.profilePic;
   const bubbleBgColor = fromMe ? 'bg-blue-500' : "";
 
 
@@ -21,8 +23,8 @@ const Message = ({message}) => {
                 />
             </div>
         </div>
-        <div className={`chat-bubble text-white ${bubbleBgColor}`}>{message.message}</div>
-        <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>12:42</div>
+        <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>{message.message}</div>
+        <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
     </div>
   )
 }
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Message from './Message'
 import useGetMessages from '../../hooks/useGetMessages'
 import MessageSkeleton from '../skeletons/MessageSkeleton';
 
 const Messages = () => {
   const {loading, messages} = useGetMessages();
-  console.log("messages", messages);
+  
+  const lastMessageRef = useRef();
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({behavior: "smooth"});
+    }, 100)
+  }, [messages])
+
   return (
     <div className='px-4 flex-1 overflow-auto'>
 
         {!loading && messages.length > 0 && messages.map((message) => (
-          <Message key={message._id} message={message} />
+          <div key={message._id} ref={lastMessageRef}> 
+            <Message message={message} />
+          </div>
         ))}
 
         {loading && [...Array(3)].map((_,idx) => <MessageSkeleton key={idx} />) }
