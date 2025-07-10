bfc529d9d052978c0e462b82aec163336a035604
yao <ycheng11036@gmail.com>
2025-07-10 18:01:42 -0400

    fix profile pic bug

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -9,7 +9,7 @@ const Message = ({message}) => {
   const fromMe = message.senderId === authUser._id;
   const formattedTime = extractTime(message.createdAt);
   const chatClassName = fromMe ? 'chat-end' : 'chat-start';
-  const profilePic = fromMe ? authUser.profielPic : selectedConversation?.profilePic;
+  const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
   const bubbleBgColor = fromMe ? 'bg-blue-500' : "";
 
   const shakeClass = message.shouldShake ? "shake" : "";
