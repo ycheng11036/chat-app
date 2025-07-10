1ef103682bb5a1053fefb0db7d7f505bb0162e02
yao <ycheng11036@gmail.com>
2025-07-10 17:30:03 -0400

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from "path";
 
 import authRoutes from './routes/auth.routes.js'; 
 import messageRoutes from './routes/message.routes.js'; 
@@ -12,6 +13,8 @@ import connectToMongoDB from './db/connectToMongoDB.js';
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); // Middleware to parse JSON bodies from req.body
@@ -21,13 +24,11 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
-
-// app.get("/", (req, res) => {
-//     // root route http://localhost:5000/
-//     res.send("Hello, World!!!!!");
-// }) 
-
+app.get("{*foo}", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 server.listen(PORT, () => {
     connectToMongoDB();
